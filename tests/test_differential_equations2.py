d57d8fadf1dd0c873b25a6420d4b5ca49bcf2ce9
Jacob Cook <jc2017@ic.ac.uk>
2023-07-31 14:24:33 +0100

    Added argument to fix broken text

--- tests/test_differential_equations2.py
+++ tests/test_differential_equations2.py
@@ -13,20 +13,39 @@ def test_f(u, expected_value):
     from microbial_thermodynamics.differential_equations2 import f
 
     calculated_value = f(t=0.0, u=u)
-    
+
     # Approx needed as floating point numbers won't be exactly the same
     assert expected_value == pytest.approx(calculated_value)
+
 
 @pytest.mark.parametrize(
     "u0, T, N, expected_t, expected_u",
     [
-        (0.1, 40, 10, [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
-        [0.1, 0.172, 0.2859328, 0.44927299, 0.6472144, 0.82987674,
-        0.94282181, 0.98594889, 0.99703183, 0.99939932, 0.99987957])
-    ]
+        (
+            0.1,
+            40,
+            10,
+            [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
+            [
+                0.1,
+                0.172,
+                0.2859328,
+                0.44927299,
+                0.6472144,
+                0.82987674,
+                0.94282181,
+                0.98594889,
+                0.99703183,
+                0.99939932,
+                0.99987957,
+            ],
+        )
+    ],
 )
 def test_forward_euler(u0, T, N, expected_t, expected_u):
-    from microbial_thermodynamics.differential_equations2 import forward_euler
-    t, u = forward_euler(u0, T, N)
+    """Test that the forward euler function works."""
+    from microbial_thermodynamics.differential_equations2 import f, forward_euler
+
+    t, u = forward_euler(f, u0, T, N)
     assert t == pytest.approx(expected_t)
     assert u == pytest.approx(expected_u)
